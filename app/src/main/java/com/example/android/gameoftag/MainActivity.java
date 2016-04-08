e2fe183e1321040060162c45646d3a8d45f273c3
Dima Dzhmil <dima.dzhmil@gmail.com>
2016-04-08 17:51:28 +0300

    ver0.0.5

--- app/src/main/java/com/example/android/gameoftag/MainActivity.java
+++ app/src/main/java/com/example/android/gameoftag/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.android.gameoftag;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.android.gameoftag.models.RulesActivity;
 
 import static java.lang.Math.*;
 
@@ -37,6 +43,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         timerText = (TextView) findViewById(R.id.timer);
         if (savedInstanceState != null) {
             headerMessage = savedInstanceState.getString("HeaderMessage");
@@ -75,6 +84,36 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_rules) {
+            Intent intent = new Intent(MainActivity.this, RulesActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        if (id == R.id.action_exit) {
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_HOME);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
